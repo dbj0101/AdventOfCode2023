32af9e4e8729d64be652bbce764ec66babeea4f1
David Brinton <dbj0101@gmail.com>
2023-12-04 21:22:10 -0500

    day4-1

--- Day4/Day4.js
+++ Day4/Day4.js
@@ -4,7 +4,44 @@ let data = process.cwd() + dir + "/data.txt";
 
 let fs = require("fs");
 let inStr = fs.readFileSync(testdata).toString("utf-8");
-//inStr = fs.readFileSync(data).toString("utf-8");
+//
+inStr = fs.readFileSync(data).toString("utf-8");
 
 let inData = inStr.split("\n");
-console.log(inData);
+//console.log(inData);
+
+function checkCard(card) {
+  //console.log(card);
+  let retVal = 0;
+  for (num of card.nums) {
+    if (card.wins.has(num)) retVal = retVal > 0 ? retVal * 2 : 1;
+  }
+  return retVal;
+}
+
+let sumTotal = 0;
+for (item of inData) {
+  console.log(item);
+  let card = {};
+  card.id = item.split(":")[0].replace("Card", "").trim();
+  card.wins = new Set();
+  item
+    .split(":")[1]
+    .split("|")[0]
+    .trim()
+    .split(" ")
+    .forEach((val) => {
+      if (val !== "") card.wins.add(Number(val));
+    });
+  card.nums = [];
+  item
+    .split(":")[1]
+    .split("|")[1]
+    .trim()
+    .split(" ")
+    .forEach((val) => {
+      if (val !== "") card.nums.push(Number(val));
+    });
+  sumTotal += checkCard(card);
+}
+console.log(sumTotal);
---
+++ Day5/Day5.js
@@ -0,0 +1,10 @@
+let dir = "/Day5";
+let testdata = process.cwd() + dir + "/testdata.txt";
+let data = process.cwd() + dir + "/data.txt";
+
+let fs = require("fs");
+let inStr = fs.readFileSync(testdata).toString("utf-8");
+//inStr = fs.readFileSync(data).toString("utf-8");
+
+let inData = inStr.split("\n");
+console.log(inData);
